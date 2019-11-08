dfbde7d797d64fc91594b3b43e3b286283a17274
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-11-08 13:00:30 -0600

    closer on styles

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from "styled-components";
 const Card = styled.div`
     box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
-    max-width: 30%;
-    background-color: orange;
-    border: 1px solid midnightblue;   
+    max-width: 300px;
+    background-color: slategrey;
+    border: 3px solid orange;   
     overflow: hidden;
     margin: 25px auto;
     border-radius: 12px;
@@ -17,16 +17,24 @@ const CardWrapper = styled.div `
     
 `;
 const MainTitle = styled.h2 `
-    color: dodgerblue;
+    color: saddlebrown;
+    margin-bottom: -10%;
 
 `;
 const Wrapper = styled.div `
     background-color: lightgray;
 `;
 const AltWrapper = styled.div `
-    background-color: lightskyblue;
-    width: 250px;
+    background-color: orange;
+    
+    
 `;
+const Text = styled.p `
+    padding: 4%;
+    font-size: 1.4rem;
+    margin-bottom: 6%;
+    color: cornsilk;
+`
 
 const CharCard = props => {
     return (
@@ -34,26 +42,21 @@ const CharCard = props => {
         <Card>
             <AltWrapper>                
             <MainTitle>Name</MainTitle>
-            {props.name}
+            <Text>{props.name}</Text>
             </AltWrapper>
             <Wrapper>
             <MainTitle>Gender</MainTitle>    
-            {props.gender}
+            <Text>{props.gender}</Text>
             </Wrapper>
             <Wrapper>
             <MainTitle>Mass</MainTitle>    
-            {props.mass}
+            <Text>{props.mass}</Text>
             </Wrapper>
             <Wrapper>
             <MainTitle>Height</MainTitle>
-            {props.height}
+            <Text>{props.height}</Text>
             </Wrapper>
-            <Wrapper>
-            {props.eye_color}
-            </Wrapper>
-            <Wrapper>
-            {props.hair_color}
-            </Wrapper>
+           
             
             
            
