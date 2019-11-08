0567361556c74b4460fb040d4f4327a4dc7cec17
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-11-08 12:19:31 -0600

    working on styling but API is erroring

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -2,46 +2,58 @@ import React from 'react';
 import styled from "styled-components";
 const Card = styled.div`
     box-shadow: 0 3px 8px rgba(0, 0, 0, 0.3);
-    max-width: 300px;
+    max-width: 30%;
     background-color: orange;
     border: 1px solid midnightblue;   
     overflow: hidden;
     margin: 25px auto;
     border-radius: 12px;
+    height: 100%;
+    
     
 `;
 const CardWrapper = styled.div `
    
     
 `;
-const MainTitle = styled.h1 `
+const MainTitle = styled.h2 `
+    color: dodgerblue;
 
-
-`
+`;
+const Wrapper = styled.div `
+    background-color: lightgray;
+`;
+const AltWrapper = styled.div `
+    background-color: lightskyblue;
+    width: 250px;
+`;
 
 const CharCard = props => {
     return (
     <CardWrapper>
         <Card>
-            <div>                
+            <AltWrapper>                
             <MainTitle>Name</MainTitle>
             {props.name}
-            </div>
-            <div>
+            </AltWrapper>
+            <Wrapper>
+            <MainTitle>Gender</MainTitle>    
             {props.gender}
-            </div>
-            <div>
+            </Wrapper>
+            <Wrapper>
+            <MainTitle>Mass</MainTitle>    
             {props.mass}
-            </div>
-            <div>
+            </Wrapper>
+            <Wrapper>
+            <MainTitle>Height</MainTitle>
             {props.height}
-            </div>
-            <div>
+            </Wrapper>
+            <Wrapper>
             {props.eye_color}
-            </div>
-            <div>
+            </Wrapper>
+            <Wrapper>
             {props.hair_color}
-            </div>
+            </Wrapper>
             
             
            
